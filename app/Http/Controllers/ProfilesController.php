c1aa00b01ceb9353b0fc81056b5d1dae34c8633c
steven-MacPro <imsyuan@gmail.com>
2020-03-26 15:31:19 +0800

    update ProfilePolicy, use correct auth to access view and btn

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -14,13 +14,13 @@ class ProfilesController extends Controller
 
     public function edit(User $user)
     {
-        $this->authorize('update', $user->profile());
+        $this->authorize('update', $user->profile);
         return view('profiles.edit', compact('user'));
     }
 
     public function update(User $user)
     {
-        $this->authorize('update', $user->profile());
+        $this->authorize('update', $user->profile);
 
         $data = request()->validate([
            'title'  =>  'required',
