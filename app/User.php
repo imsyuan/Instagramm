c6ae63bda71c183c7d28a46d731b02dbba6b8bb6
steven-MacPro <imsyuan@gmail.com>
2020-04-10 17:23:58 +0800

    #create - Sending Emails to New Registered Users

--- app/User.php
+++ app/User.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
+use App\Mail\NewUserWelcomeMail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
 
 class User extends Authenticatable
 {
@@ -51,6 +53,8 @@ class User extends Authenticatable
             $user->profile()->create([
                 'title' => $user->username,
             ]);
+
+            Mail::to($user->email)->send(new NewUserWelcomeMail());
         });
     }
 
